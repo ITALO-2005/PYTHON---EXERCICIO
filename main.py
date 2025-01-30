6b3d4266cbf054a4ed071bd26ae9965937eb4b9e
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-01-30 16:47:47 +0000

    TESTE

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+#QUESTÃO 1
+def saudacao(nome):
+    print(f"Olá, {nome}!")
+saudacao('Ítalo')
+
+#QUESTÃO 2
+def dobro(numero):
+    return numero * 2
+resultado = dobro(5)
+print(resultado)
+
+#QUESTÃO 3
+def saudacao_personalizada(nome, idioma):
+    idioma = ingles
+    return nome
+print(ingles)
+
+#QUESTÃO 4
+def potencia(base, expoente = 2):
+    return base ** expoente
+
+print(potencia(5), potencia(6, 3), potencia(10, 4))       
+
+#QUESTÃO 9
+def calcular_pagamento(horastrabalhadas, taxa_hora):
+    pagamento_total = horastrabalhadas * taxa_hora
+    return pagamento_total
+horas = 40
+taxa = 20
+total = calcular_pagamento(horas, taxa)
+print(total)
 
